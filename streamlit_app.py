85b92242cd0e88c8e3c5743ca15d6c497b247a26
suntravel <lv.luca@gmail.com>
2025-08-06 15:41:09 +0800

    Update streamlit_app.py
    Connected Changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -9,6 +9,10 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
 )
+
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 #option = st.selectbox(
 #    "How would you like to be contacted?",
